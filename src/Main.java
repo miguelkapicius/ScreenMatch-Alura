8f5e0cab134db921f87798f35c53c4b512ed397f
Miguel Kapicius Caires <miguelkapicius@gmail.com>
2024-04-02 23:17:01 -0300

    Aula 01 finalizada

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        Movie myMovie = new Movie();
+
+        myMovie.name = "Top Gun: Maverick";
+        myMovie.releaseYear = 2022;
+        myMovie.include = true;
+        myMovie.rating = 0;
+        myMovie.totalRatings = 0;
+        myMovie.duration = 120;
+
+        myMovie.displaySheet();
+        myMovie.rateMovie(8);
+        myMovie.rateMovie(5);
+        myMovie.rateMovie(10);
+
+        System.out.println(myMovie.totalRatings);
+        System.out.println(myMovie.rating);
+        System.out.println(myMovie.ratingAverage());
+    }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,22 @@
 public class Movie {
+    String name;
+    int releaseYear;
+    boolean include;
+    double rating;
+    int totalRatings;
+    int duration;
+
+    void displaySheet() {
+        System.out.println("Movie Name: " + name);
+        System.out.println("Release Year: " + releaseYear);
+    }
+
+    void rateMovie(double grade) {
+        rating += grade;
+        totalRatings++;
+    }
+
+    double ratingAverage() {
+        return rating / totalRatings;
+    }
 }
