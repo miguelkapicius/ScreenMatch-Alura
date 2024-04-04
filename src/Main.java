679c4274242e703ac60e632064f6ce4da8301efa
Miguel Kapicius Caires <miguelkapicius@gmail.com>
2024-04-04 11:36:37 -0300

    ...

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,20 @@
+import br.com.alura.screenmatch.models.Movie;
+
 public class Main {
     public static void main(String[] args) {
         Movie myMovie = new Movie();
 
-        myMovie.name = "Top Gun: Maverick";
-        myMovie.releaseYear = 2022;
-        myMovie.include = true;
-        myMovie.rating = 0;
-        myMovie.totalRatings = 0;
-        myMovie.duration = 120;
+        myMovie.setName("Top Gun: Maverick");
+        myMovie.setReleaseYear(2022);
+        myMovie.setInclude(true);
+        myMovie.setDuration(128);
 
         myMovie.displaySheet();
         myMovie.rateMovie(8);
         myMovie.rateMovie(5);
         myMovie.rateMovie(10);
 
-        System.out.println(myMovie.totalRatings);
-        System.out.println(myMovie.rating);
+        System.out.println("Total Ratings: " + myMovie.getTotalRatings());
         System.out.println(myMovie.ratingAverage());
     }
 }
--- src/br/com/alura/screenmatch/models/Movie.java
+++ src/br/com/alura/screenmatch/models/Movie.java
@@ -1,26 +1,60 @@
+package br.com.alura.screenmatch.models;
+
 public class Movie {
-    String name;
-    int releaseYear;
-    boolean include;
+    private String name;
+    private int releaseYear;
+    private boolean include;
     private double rating;
     private int totalRatings;
-    int duration;
+    private int duration;
 
-    int getTotalRatings() {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setReleaseYear(int releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public void setInclude(boolean include) {
+        this.include = include;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getTotalRatings() {
         return totalRatings;
     }
 
-    void displaySheet() {
-        System.out.println("Movie Name: " + name);
+    public String getName() {
+        return name;
+    }
+
+    public boolean isInclude() {
+        return include;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public int getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void displaySheet() {
+        System.out.println("br.com.alura.screenmatch.models.Movie Name: " + name);
         System.out.println("Release Year: " + releaseYear);
     }
 
-    void rateMovie(double grade) {
+    public void rateMovie(double grade) {
         rating += grade;
         totalRatings++;
     }
 
-    double ratingAverage() {
+    public double ratingAverage() {
         return rating / totalRatings;
     }
 }
